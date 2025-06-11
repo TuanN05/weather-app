32079aeec202abb2142aa705ae9751edeb44659e
imnonamw999 <qpeffff@gmail.com>
2025-06-11 12:20:22 +0700

    fix app

--- index.js
+++ index.js
@@ -14,18 +14,65 @@ const windSpeed = document.querySelector('.wind-speed');
 
 
 searchInput.addEventListener('change', (e) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}&units=metric&lang=vi`)
-        .then(async res => {
-            const data = await res.json();
-            console.log('[Search Input]', data);
-            cityName.innerHTML = data.name || DEFAULT_VALUE;
-            weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
-            weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-            temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+    const location = e.target.value.trim();
 
-            sunrise.innerHTML = moment.unix(data.sys.sunrise).format('H:mm') || DEFAULT_VALUE;
-            sunset.innerHTML = moment.unix(data.sys.sunset).format('H:mm') || DEFAULT_VALUE;
-            humidity.innerHTML = data.main.humidity || DEFAULT_VALUE;
-            windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
+    // Bước 1: Gọi Geocoding API
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${APP_ID}`)
+        .then(res => res.json())
+        .then(geoData => {
+            if (!geoData || geoData.length === 0) {
+                alert('Không tìm thấy vị trí. Vui lòng nhập lại.');
+                return;
+            }
+
+            const { lat, lon, name, state, country } = geoData[0];
+
+            // Gọi API weather hiện tại
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APP_ID}&units=metric&lang=vi`)
+                .then(res => res.json())
+                .then(data => {
+                    cityName.innerHTML = `${name}, ${state || ''}`;
+                    weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+                    weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+                    temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+
+                    sunrise.innerHTML = moment.unix(data.sys.sunrise).format('H:mm') || DEFAULT_VALUE;
+                    sunset.innerHTML = moment.unix(data.sys.sunset).format('H:mm') || DEFAULT_VALUE;
+                    humidity.innerHTML = data.main.humidity || DEFAULT_VALUE;
+                    windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
+                });
+
+            // Gọi API dự báo
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APP_ID}&units=metric&lang=vi`)
+                .then(res => res.json())
+                .then(data => {
+                    const forecastList = data.list;
+                    const dailyData = {};
+
+                    forecastList.forEach(item => {
+                        const date = item.dt_txt.split(' ')[0];
+                        const hour = item.dt_txt.split(' ')[1];
+                        if (hour === '12:00:00') {
+                            dailyData[date] = item;
+                        }
+                    });
+
+                    const forecastContainer = document.querySelector('.forecast-list');
+                    forecastContainer.innerHTML = ''; // clear old
+
+                    Object.keys(dailyData).forEach(date => {
+                        const item = dailyData[date];
+                        const card = document.createElement('div');
+                        card.className = 'forecast-card';
+                        const day = moment(date).format('dddd');
+                        card.innerHTML = `
+                            <div>${day}</div>
+                            <img src="http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png" alt="weather">
+                            <div>${Math.round(item.main.temp)}°C</div>
+                            <div>${item.weather[0].description}</div>
+                        `;
+                        forecastContainer.appendChild(card);
+                    });
+                });
         });
 });
